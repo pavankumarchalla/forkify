d61bfdd66ac8bda9c8442ae5c76fd0eff144a913
Pavan Kumar C <pavan.challa23@gmail.com>
2018-11-28 16:20:05 +0530

    search model implemented

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,6 @@
-//57087664a44a75006b80945cd6bc4da0
+import Search from './models/Search';
 
-//https://www.food2fork.com/api/search
+const search = new Search('pizza');
+console.log(search)
 
-import axios from 'axios';
-
-async function getResults(query) {
-    try {
-        const key = '57087664a44a75006b80945cd6bc4da0';
-        const result = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = result.data.recipes;
-        console.log(recipes);
-    } catch (error) {
-        alert(error);
-    }
-
-}
-
-getResults('pizza');
+search.getResults();
---
+++ src/js/models/Search.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export default class Search {
+
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const key = '57087664a44a75006b80945cd6bc4da0';
+
+        try {
+            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            console.log(this.result);
+        } catch (error) {
+            alert(error)
+        }
+    
+    }
+    
+}
