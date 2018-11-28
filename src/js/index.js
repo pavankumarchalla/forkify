036ce91752dda81ef04085608c70295c9996f477
Pavan Kumar C <pavan.challa23@gmail.com>
2018-11-28 18:11:36 +0530

    rendered an ajax loading spinner

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView'
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 const state = {};
 
@@ -11,11 +11,15 @@ const controlSearch = async () => {
         state.search = new Search(query);
 
         searchView.clearInput();
-        
+
         searchView.clearResults();
+
+        renderLoader(elements.searchRes);
 
         await state.search.getResults();
 
+        clearLoader();
+        
         searchView.renderResults(state.search.result);
     }
 }
