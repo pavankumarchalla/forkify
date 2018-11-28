12d620356e9ba9c0bae6acfa8e7695de4bd790af
Pavan Kumar C <pavan.challa23@gmail.com>
2018-11-28 16:41:41 +0530

    Search submit handled

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,22 @@
 import Search from './models/Search';
 
-const search = new Search('pizza');
-console.log(search)
+const state = {};
 
-search.getResults();
+const controlSearch = async () => {
+    const query = 'pizza'; //TODO
+
+    if (query) {
+        state.search = new Search(query);
+
+        await state.search.getResults();
+
+        console.log(state.search.result);
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+const search = new Search('pizza');
